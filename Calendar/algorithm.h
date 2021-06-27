40d545eedf968e27d058845cfa2deed88f7ffd05
윤성구 <otj5894@live.wsu.ac.kr>
2021-06-27 16:44:21 +0900

    get_week 주석 추가

--- Calendar/algorithm.h
+++ Calendar/algorithm.h
@@ -4,7 +4,7 @@
 
 char temp_string[MAX_STRING_SIZE];
 
-int get_week(int date[3]);
+int get_week(int date[3]); // {년,월,일}을 인자로 주면 0~6까지(일~토)의 값을 반환
 char get_choice();
 const char* get_string();
 int str_seatch(const char* src, const char* value);
